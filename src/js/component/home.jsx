738fc9223f002b091055d592ef4b861f5626e711
Mermate <138124998+Mermate@users.noreply.github.com>
2023-11-14 22:00:21 +0000

    First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,58 @@
-import React from "react";
+import React ,{useState} from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const ToDoList = () => {
+
+    const [inputValue, setInputValue] = useState("");
+    const [toDos, setToDos] = useState([]);
+
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter" && inputValue !== "") {
+            // add the new task to the list
+            setToDos([...toDos, inputValue]);
+            setInputValue("");
+        }
+    };
+    const handleDelete = (index) => {
+        // Remove the task at the specified index
+        setToDos(toDos.filter((item, currentIndex) => index !== currentIndex));
+      };
+
+
+    return (
+        <div className="container">
+            <h1 className="title">ToDos</h1>
+
+            <ul>
+                <li>
+                    <input
+                        className="inputList"
+                        type="text"
+                        onChange={(e) => setInputValue(e.target.value)}
+                        value={inputValue}
+                        onKeyDown={handleKeyDown}
+                        placeholder={toDos.length === 0 ? 'No tasks, add a task' : ''}
+                    />
+                </li>
+
+				{toDos.map((item, index) => (
+                    <li key={index}>
+                          {item}
+                    <span><button className="deleteButton" onClick={() => handleDelete(index)}>X</button>
+                      
+                    </span>
+                          
+                    </li>
+                ))}
+            </ul>
+						 
+        <div className="bottomList">{toDos.length} items left</div>
+        </div>
+    );
 };
 
-export default Home;
+export default ToDoList;
